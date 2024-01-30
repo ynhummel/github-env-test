2109f59b3123a796c4748eb23fee2b1b0bfe37d8
Yuri Hummel <yurihummel@outlook.com>
2024-01-30 17:51:24 -0300

    adjust env

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load(".version")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	version := os.Getenv("LATEST_VERSION")
+	if version == "" {
+		version = "versionNotFound"
+	}
 
 	fmt.Printf("The release version is: %s\n", version)
 }
